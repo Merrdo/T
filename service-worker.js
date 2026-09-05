365f12c9b1edf5d5e3f7a33657a2cfbfe87e8aff
Merrdo <mertkutlu26@gmail.com>
2026-09-05 20:19:23 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // Böylece GitHub'a her yeni sürüm yüklendiğinde kullanıcı hep güncel içeriği görür;
 // internet yoksa en son önbelleklenen sürüm gösterilir.
 
-const CACHE_VERSION = 'takvimim-v3'; // Her önemli güncellemede bu numarayı artırın (v3, v4, ...)
+const CACHE_VERSION = 'takvimim-v3'; // Her önemli güncellemede bu numarayı artırın (v4, v5, ...)
 const CORE_ASSETS = [
   './',
   './index.html',
